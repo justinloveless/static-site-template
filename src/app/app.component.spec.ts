e573b405128865798cb7a16573a0886b3c79e1d6
justinlovelessx <justinlovelessx@gmail.com>
2024-02-12 23:57:05 -0600

    replace all references to gen with template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'static-site-gen'`, () => {
+  it(`should have as title 'static-site-template'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('static-site-gen');
+    expect(app.title).toEqual('static-site-template');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('static-site-gen app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('static-site-template app is running!');
   });
 });
